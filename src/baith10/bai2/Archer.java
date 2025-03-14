8f60bd4b794b825ee941531204a8e34f0fc98d88
Phạm Hùng <116713724+kaminokokoro@users.noreply.github.com>
2025-03-14 16:41:30 +0700

    first commit

--- src/baith10/bai2/Archer.java
+++
@@ -1,9 +0,0 @@
-package baith10.bai2;
-
-public class Archer extends Character {
-    public Archer(String name) {
-        super(name);
-        this.weapon = new BowBehavior(); // Mặc định dùng cung
-    }
-}
-
